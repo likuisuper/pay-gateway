ef07783c44c123c6c8f069cd55d77dc8af2cd0b6
xhx <312263441@qq.com>
2022-08-16 15:11:19 +0800

    字节支付回调

--- rpc/internal/config/config.go
+++ rpc/internal/config/config.go
@@ -8,41 +8,7 @@ import (
 type Config struct {
 	zrpc.RpcServerConf
 	Mysql []*db.DbConfig `json:"Mysql"`
-	////支付配置
-	//Alipay    []*AliPay
-	//WeChatPay []*WechatPay
-	//TikTokPay []*TikTokPay
-
-	//AppRel []*AppRelConfig //包名对应的支付配置
 }
-
-////支付宝参数
-//type AliPay struct {
-//	AppId            string
-//	PrivateKey       string
-//	PublicKey        string
-//	AppCertPublicKey string
-//	PayRootCert      string
-//	IsProduction     bool
-//}
-//
-////字节支付参数
-//type TikTokPay struct {
-//	AppId     string //应用ID
-//	SALT      string //加密参数
-//	NotifyUrl string //通知地址
-//	Token     string //token
-//}
-//
-////微信支付参数
-//type WechatPay struct {
-//	AppId          string //应用ID
-//	MchId          string //直连商户号
-//	ApiKey         string //apiV3密钥
-//	PrivateKeyPath string //apiV3密钥
-//	SerialNumber   string //商户证书序列号
-//	NotifyUrl      string //通知地址
-//}
 
 // mysql配置
 type DbConfig struct {
