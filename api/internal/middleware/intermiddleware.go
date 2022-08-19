7c7209ae2713001d02caed3132ce80009cf51e06
xhx <312263441@qq.com>
2022-08-19 22:25:09 +0800

    inter middleware

--- api/internal/middleware/intermiddleware.go
+++ api/internal/middleware/intermiddleware.go
@@ -1,9 +1,6 @@
 package middleware
 
 import (
-	"errors"
-	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
 	"strings"
 )
@@ -17,17 +14,19 @@ func NewInterMiddleware() *InterMiddleware {
 
 func (m *InterMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		allowCidr := "172.30.0.0/16"
-		ip := r.Header.Get("X-Forwarded-For")
-		allow := m.isBelong(ip, allowCidr)
-		if allow {
-			next(w, r)
-			return
-		}
-		err := errors.New("not allow")
-		logx.Errorf("InterMiddleware err: %v", err)
-		httpx.Error(w, err)
-		return
+		//allowCidr := "172.30.0.0/16"
+		//ip := r.Header.Get("X-Forwarded-For")
+		//allow := m.isBelong(ip, allowCidr)
+		//if allow {
+		//	next(w, r)
+		//	return
+		//}
+		//err := errors.New("not allow")
+		//logx.Errorf("InterMiddleware err: %v", err)
+		//httpx.Error(w, err)
+		//return
+
+		next(w, r)
 	}
 }
 
