1e3792156d756ec6be099c011ee22f4707ad5231
xhx <312263441@qq.com>
2022-08-20 09:47:02 +0800

    inter middleware

--- api/internal/middleware/intermiddleware.go
+++ api/internal/middleware/intermiddleware.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"errors"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
 	"strings"
 )
@@ -19,20 +21,23 @@ func (m *InterMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 		ip := r.Header.Get("X-Forwarded-For")
 		logx.Info("InterMiddleware X-Forwarded-For: %s", ip)
 		logx.Info("InterMiddleware remote-addr: %s", r.RemoteAddr)
+		//如果直接ip+端口访问取不到ip
+		if ip == "" {
+			ip = r.RemoteAddr
+		}
 
-		//allowCidr := "172.30.0.0/16"
-		//ip := r.Header.Get("X-Forwarded-For")
-		//allow := m.isBelong(ip, allowCidr)
-		//if allow {
-		//	next(w, r)
-		//	return
-		//}
-		//err := errors.New("not allow")
-		//logx.Errorf("InterMiddleware err: %v", err)
-		//httpx.Error(w, err)
-		//return
+		allowCidr := "172.30.0.0/16"
+		allow := m.isBelong(ip, allowCidr)
+		if allow {
+			next(w, r)
+			return
+		}
+		err := errors.New("not allow")
+		logx.Errorf("InterMiddleware err: %v", err)
+		httpx.Error(w, err)
+		return
 
-		next(w, r)
+		//next(w, r)
 	}
 }
 
