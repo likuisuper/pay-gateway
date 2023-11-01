ca8f3f8578cc14cf293d90dbb6e557769ab4226f
YanYiXin <450005215@qq.com>
2023-11-01 21:26:40 +0800

    fix 自动扣款失败

--- common/types/product.go
+++ common/types/product.go
@@ -1,14 +1,15 @@
 package types
 
 type Product struct {
+	Id              int     `json:"id"`
 	ProductId       string  `json:"product_id"`
 	ProductName     string  `json:"product_name"`
-	ProductType     int     `json:"product_type"`
 	Amount          float64 `json:"amount"`
-	PrepaidAmount   float64  `json:"prepaid_amount"`
-	SubscribePeriod int     `json:"subscribe_period"`
-	VipDays         int     `json:"vip_day"`
+	ProductType     int     `json:"product_type"`
+	VipDay          int     `json:"vip_day"`
 	TopText         string  `json:"top_text"`
 	BottomText      string  `json:"bottom_text"`
+	PrepaidAmount   float64 `json:"prepaid_amount"`
 	TryVipDay       int     `json:"try_vip_day"`
+	SubscribePeriod int     `json:"subscribe_period"`
 }
