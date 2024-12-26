5248940d39dfbcab32e623159bdf61f04fd18ea7
dujunwen <dujunwen@muchcloud.com>
2024-12-26 09:27:50 +0800

    添加快手支付ios

--- common/client/ksPay.go
+++ common/client/ksPay.go
@@ -27,13 +27,46 @@ var (
 // 快手开发接口文档
 // https://open.kuaishou.com/docs/develop/server/epay/open-api-new/prePay-new.html
 
+// =============================IOS支付说明=============================
+// https://open.kuaishou.com/docs/develop/server/iosEpayAbility/iosEpayGuide.html
+// 快手小程序接入iOS的IAP支付，相较于担保支付(单次) (opens new window)，有两处变动改造点
+// 使用/openapi/mp/developer/epay/iap/create_order预下单接口
+// ks.pay接口增加苹果内购标识
+//
+// 苹果内购不支持线上退款，因此没有退款请求接口。若用户有退款请求，可使用以下方式
+// 商家可联系用户线下协商退款
+// 引导用户至苹果客服侧申请退款
+// 1.3 关于订单同步
+// IAP支付的订单需要进行订单同步，使用订单信息同步接口(opens new window)
+//
+// 1.4 关于账单查询
+// 与担保支付的账单查询接口保持一致：担保支付|账单查询能力(opens new window)
+//
+// 支付方式为APPLE_PAY的订单，即为苹果支付的订单
+//
+// IAP订单会在支付账单里面添加
+//
+// 订单原价
+// 用户支付金额
+// 平台补贴金额
+// 支付渠道：APPLE_PAY
+// 在退款账单新增：
+//
+// 支付渠道：APPLE_PAY
+// 1.5 版本说明
+// 在iOS系统中，快手11.6.50版本开始封禁小程序支付能力，无法通过微信或者支付宝支付。
+//
+// 在12.0.20版本中支持苹果IAP支付，因此在接入IAP支付时，需要把测试机版本升级到12.0.20及以上
+// =====================================================================
+
 // 请求地址
 const (
-	ksAccessToken            = "https://open.kuaishou.com/oauth2/access_token"                                 //获取accessToken
-	KsCreateOrderWithChannel = "https://open.kuaishou.com/openapi/mp/developer/epay/create_order_with_channel" //预下单接口（无收银台版）
-	KsCreateOrder            = "https://open.kuaishou.com/openapi/mp/developer/epay/create_order"              //预下单接口（有收银台版）
-	KsCancelChannel          = "https://open.kuaishou.com/openapi/mp/developer/epay/cancel_channel"            //取消支付方式
-	KsQueryOrder             = "https://open.kuaishou.com/openapi/mp/developer/epay/query_order"               //查询订单
+	ksAccessToken            = "https://open.kuaishou.com/oauth2/access_token"                                 // 获取accessToken
+	KsCreateOrderWithChannel = "https://open.kuaishou.com/openapi/mp/developer/epay/create_order_with_channel" // 预下单接口安卓（无收银台版）
+	KsCreateOrder            = "https://open.kuaishou.com/openapi/mp/developer/epay/create_order"              // 预下单接口安卓（有收银台版）
+	KsCreateOrderIos         = "https://open.kuaishou.com/openapi/mp/developer/epay/iap/create_order"          // 预下单接口苹果（有收银台版） 注：IAP支付接入，仅因对iOS系统内，安卓系统无需关注，使用老的担保支付即可
+	KsCancelChannel          = "https://open.kuaishou.com/openapi/mp/developer/epay/cancel_channel"            // 取消支付方式
+	KsQueryOrder             = "https://open.kuaishou.com/openapi/mp/developer/epay/query_order"               // 查询订单
 )
 
 type KsPayConfig struct {
@@ -65,6 +98,19 @@ type KsCreateOrderReq struct {
 	ExpireTime  int    `json:"expire_time"`  //订单过期时间，单位秒，300s - 172800s
 	Sign        string `json:"sign"`         //签名
 	NotifyUrl   string `json:"notify_url"`   //通知URL，不允许携带查询串
+}
+
+type KsCreateOrderReqIos struct {
+	OutOrderNo    string `json:"out_order_no"`    //商户系统内部订单号
+	OpenId        string `json:"open_id"`         //快手用户在当前小程序的open_id
+	UserPayAmount int    `json:"user_pay_amount"` //用户实际金额，单位为[分]
+	OrderAmount   int    `json:"order_amount"`    //订单原价，单位为[分]，不允许传非整数的数值。
+	Subject       string `json:"subject"`         //商品描述
+	Detail        string `json:"detail"`          //商品详情
+	Type          int    `json:"type"`            //商品类型，不同商品类目的编号
+	ExpireTime    int    `json:"expire_time"`     //订单过期时间，单位秒，300s - 172800s
+	Sign          string `json:"sign"`            //签名
+	NotifyUrl     string `json:"notify_url"`      //通知URL，不允许携带查询串
 }
 
 type KsProvider struct {
@@ -193,7 +239,7 @@ func (p *KsPay) CreateOrderWithChannel(info *PayOrder, openId string) (respData 
 	return
 }
 
-// 预下单接口(有收银台版)
+// 预下单接口安卓(有收银台版)
 func (p *KsPay) CreateOrder(info *PayOrder, openId string) (respData *KsCreateOrderWithChannelResp, err error) {
 	accessToken, err := p.GetAccessTokenWithCache()
 	if err != nil {
@@ -212,6 +258,58 @@ func (p *KsPay) CreateOrder(info *PayOrder, openId string) (respData *KsCreateOr
 		Type:        info.KsTypeId,
 		ExpireTime:  3600, // 订单过期时间，单位秒，300s - 172800s
 		NotifyUrl:   p.Config.NotifyUrl,
+	}
+	param.Sign = p.Sign(param)
+
+	dataStr, err := util.HttpPost(uri, param, 3*time.Second)
+	if err != nil {
+		jsonStr, _ := jsoniter.MarshalToString(param)
+		util.CheckError("CreateOrder Err:%v, dataJson: %s", err, jsonStr)
+		ksHttpRequestErr.CounterInc()
+		return
+	}
+
+	resultCode := jsoniter.Get([]byte(dataStr), "result").ToInt()
+	if resultCode != 1 {
+		errorMsg := jsoniter.Get([]byte(dataStr), "error_msg").ToString()
+		err = errors.New(errorMsg)
+		jsonStr, _ := jsoniter.MarshalToString(param)
+		util.CheckError("CreateOrderWithChannel Err:%v, dataJson: %s", err, jsonStr)
+		ksHttpRequestErr.CounterInc()
+		return
+	}
+
+	respData = new(KsCreateOrderWithChannelResp)
+	jsoniter.Get([]byte(dataStr), "order_info").ToVal(respData)
+
+	logx.Slowf("KsPay-CreateOrder, param:%+v, dataStr:%s", param, dataStr)
+	return
+}
+
+// 预下单接口苹果ios
+// https://open.kuaishou.com/docs/develop/server/iosEpayAbility/iosEpayGuide.html
+//
+// 苹果价格档位
+// https://open.kuaishou.com/docs/develop/server/iosEpayAbility/feeStandards.html
+func (p *KsPay) CreateOrderIos(info *PayOrder, openId string) (respData *KsCreateOrderWithChannelResp, err error) {
+	accessToken, err := p.GetAccessTokenWithCache()
+	if err != nil {
+		util.CheckError("CreateOrderIos GetAccessTokenWithCache Err:%v", err)
+		return
+	}
+
+	uri := fmt.Sprintf("%s?app_id=%s&access_token=%s", KsCreateOrderIos, p.Config.AppId, accessToken)
+
+	param := &KsCreateOrderReqIos{
+		OutOrderNo:    info.OrderSn,
+		OpenId:        openId,
+		UserPayAmount: info.Amount, // 用户实际金额，单位为[分] 如果订单没有补贴，则user_pay_amount=order_amount
+		OrderAmount:   info.Amount, // 必填
+		Subject:       info.Subject,
+		Detail:        info.Subject,
+		Type:          info.KsTypeId,
+		ExpireTime:    3600, // 订单过期时间，单位秒，300s - 172800s
+		NotifyUrl:     p.Config.NotifyUrl,
 	}
 	param.Sign = p.Sign(param)
 
