7c861d5c1f991ba2fd63a4ebbea78835a49de60e
dujunwen <dujunwen@muchcloud.com>
2025-01-19 17:29:57 +0800

    修改华为普通商品支付回调

--- api/internal/logic/notify/notifyhuaweilogic.go
+++ api/internal/logic/notify/notifyhuaweilogic.go
@@ -46,7 +46,7 @@ func NewNotifyHuaweiLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Noti
 // 线上环境回调地址: https://pay-gw.muchcloud.com/notify/huawei
 func (l *NotifyHuaweiLogic) NotifyHuawei(req *types.HuaweiReq) *huawei.NotificationResponse {
 	jsonByte, _ := json.Marshal(req)
-	l.Sloww("华为回调记录", logx.Field("data", req), logx.Field("json", string(jsonByte)))
+	l.Sloww("华为回调记录", logx.Field("req", req), logx.Field("json", string(jsonByte)))
 
 	if req.EventType == "" {
 		// 配置的时候保存的回调 不处理 记录一下日志就可以了
@@ -231,6 +231,7 @@ func (l *NotifyHuaweiLogic) handleHuaweiSub(req *types.HuaweiReq, hwApp *model.H
 	}
 
 	// 续费的时候需要回调
+	// 异步回调
 	// TODO:
 
 	return nil
@@ -241,5 +242,74 @@ func (l *NotifyHuaweiLogic) handleHuaweiSub(req *types.HuaweiReq, hwApp *model.H
 // 6.调用华为IAP服务器提供的Order服务确认购买接口确认购买（即消耗）。
 // 7.IAP服务器返回确认购买结果。
 func (l *NotifyHuaweiLogic) handleHuaweiOrder(req *types.HuaweiReq, hwApp *model.HuaweiAppTable, logId int) error {
+	// 订单 notificationType 通知事件的类型，取值如下：1：支付成功 2：退款成功
+	if req.OrderNotification.NotificationType != 1 {
+		// 非支付成功直接返回
+		l.Errorw("订单非支付成功", logx.Field("req", req))
+		return nil
+	}
+
+	purchaseToken := req.OrderNotification.PurchaseToken
+	if purchaseToken == "" {
+		err := errors.New("订单购买token为空")
+		l.Error(err.Error())
+		return err
+	}
+
+	// 根据购买token查找订单数据
+	hworder, err := l.huaweiOrderModel.GetOneByToken(purchaseToken)
+	if err != nil {
+		l.Errorf("GetOneByToken error: %v, token: %s", err, purchaseToken)
+		return err
+	}
+
+	if hworder == nil || hworder.Id < 1 {
+		err = errors.New("获取订单失败")
+		l.Error(err.Error() + " 订单为空, token: " + purchaseToken)
+		return err
+	}
+
+	if hworder.Status != 0 {
+		// 订单已处理
+		l.Slowf("订单已处理 purchase_token: %s, order id: %d", purchaseToken, hworder.Id)
+		return nil
+	}
+
+	// 验证数据
+	if hworder.ProductId != req.OrderNotification.ProductId {
+		err = errors.New("商品id不一致")
+		l.Errorf(err.Error()+" 数据库商品id: %s, 回传商品id: %s", hworder.ProductId, req.OrderNotification.ProductId)
+		return err
+	}
+
+	if hworder.AppId != req.ApplicationId {
+		err = errors.New("应用id不一致")
+		l.Errorf(err.Error()+" 数据库app id: %s, 回传app id: %s", hworder.AppId, req.ApplicationId)
+		return err
+	}
+
+	// 更新数据
+	var purchaseTime string
+	if req.NotifyTime > 1000 {
+		purchaseTime = time.Unix(int64(req.NotifyTime/1000), 0).Format("2006-01-02 15:04:05")
+	}
+	updateData := map[string]interface{}{
+		"log_id":            logId,
+		"version":           req.Version,
+		"event_type":        req.EventType,
+		"notify_time":       int(req.NotifyTime / 1000), // 毫秒转成秒级时间戳
+		"notification_type": req.OrderNotification.NotificationType,
+		"environment":       "",
+		"status":            1, // TODO: 还有退款等其他
+		"pay_time":          purchaseTime,
+	}
+	err = l.huaweiOrderModel.UpdateData(hworder.Id, updateData)
+	if err != nil {
+		return err
+	}
+
+	// TODO:
+	// 异步回调
+
 	return nil
 }
